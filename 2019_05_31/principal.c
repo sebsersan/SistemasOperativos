e278e49e574e18d64be80ce2fa819b01a6f7ec06
Your Name <you@example.com>
2019-05-31 11:38:11 -0500

    adicionando ejemplo archivo cabecera

---
+++ 2019_05_31/principal.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "entero.h"
+
+int main(int argc, char** argv) {
+  entero a,b;
+
+  a = 5;
+  b = 6;
+  printf("la suma de %d + %d es %d\n",a,b,suma(a,b));
+  printf("la resta de %d - %d es %d\n",a,b,resta(a,b));
+  printf("la multiplicacion de %d * %d es %d\n",a,b,multiplicacion(a,b));
+  printf("la division de %d * %d es %f\n",a,b,(float)division(a,b));
+
+}
+
